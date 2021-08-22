ae1195c8abdd835e687a69d167b78c3fa26beff0
Sergiy Bubenko <bubenko.s91@gmail.com>
2021-08-22 16:43:06 +0300

    ok

--- src/index.js
+++ src/index.js
@@ -10,29 +10,24 @@ const colors = [
 ];
 
 let id = null;
-
 const refs = {
   start: document.querySelector('button[data-action="start"]'),
   stop: document.querySelector('button[data-action="stop"]'),
 };
 
-
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
-
 function randomBgClr() {
   document.body.style.backgroundColor =
     colors[randomIntegerFromInterval(1, colors.length)];
 };
-
 
 function startRandomBgClr() {
   refs.start.disabled = true
   refs.start.classList.toggle('active')
   id = setInterval(randomBgClr, 1000);
 };
-
 function stopRandomBgClr() {
   refs.start.disabled = false;
   refs.start.classList.remove('active')
